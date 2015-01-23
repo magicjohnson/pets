b9850d87bacd1365ea1d95099eae1f4e8f4a9d00
Dmitriy Trochshenko <dmitriy.trochshenko@gmail.com>
2015-01-23 20:27:12 +0600

    initial commit

---
+++ pets/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = patterns('',
+    url(r'^', include('main.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
