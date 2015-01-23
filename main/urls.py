b22183fb6ca331a00eb28e7acebb78bf0d53845b
Dmitriy Trochshenko <dmitriy.trochshenko@gmail.com>
2015-01-24 01:15:36 +0600

    pet detail view

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,8 @@ from main import views
 
 urlpatterns = patterns('',
     url(r'^$', views.HomeView.as_view()),
-    url(r'^cats/$', views.CatsView.as_view()),
-    url(r'^dogs/$', views.DogsView.as_view()),
+    url(r'^cats/$', views.CatListView.as_view(), name='cat-list'),
+    url(r'^dogs/$', views.DogListView.as_view(), name='dog-list'),
+    url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'pets/(?P<slug>[\w-]+)', views.PetDetailView.as_view(), name='pet-detail')
 )
--- main/views.py
+++ main/views.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from django.views.generic import TemplateView, ListView
-# from django.views.decorators.csrf import csrf_protect
-# from django.shortcuts import render_to_response
-# from django.template import RequestContext
-from models import Pet
+from django.views import generic
 
-# Create your views here.
+from main.models import Pet
 
 
-class HomeView(TemplateView):
+class HomeView(generic.TemplateView):
     template_name = 'home.html'
 
 
-class CatsView(ListView):
+class CatListView(generic.ListView):
     model = Pet
     context_object_name = 'pets'
     template_name = 'pet_list.html'
@@ -23,12 +19,16 @@ class CatsView(ListView):
         return Pet.objects.filter(animal=0)
 
 
-class DogsView(ListView):
+class DogListView(generic.ListView):
     model = Pet
     context_object_name = 'pets'
     template_name = 'pet_list.html'
     paginate_by = 10
 
     def get_queryset(self):
-        return Pet.objects.filter(animal=1)
-        
+        return Pet.objects.filter(animal=Pet.DOG)
+
+
+class PetDetailView(generic.DetailView):
+    template_name = 'pet_detail.html'
+    model = Pet
