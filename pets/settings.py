0a511d4e95ec9280f79c04aad7cdc0ca2ff37775
Dmitriy Trochshenko <dmitriy.trochshenko@gmail.com>
2015-01-24 12:48:46 +0600

    integrated waffle

--- pets/settings.py
+++ pets/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'waffle',
     'main',
 )
 TEMPLATE_DIRS = (
@@ -65,6 +66,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'waffle.middleware.WaffleMiddleware',
 )
 
 ROOT_URLCONF = 'pets.urls'
