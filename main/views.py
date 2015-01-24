5a858fb2a495c8712266de6adfe52c836e734da0
Yevgeniy Valeyev <z.mazay@gmail.com>
2015-01-24 18:29:56 +0600

    Search form for home page (visible part).

--- main/views.py
+++ main/views.py
@@ -2,10 +2,16 @@
 
 from django.views import generic
 from main.models import Pet
+from main.forms import SearchForm
 
 
 class HomeView(generic.TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        context['form'] = SearchForm()
+        return context
 
 
 class PetDetailView(generic.DetailView):
@@ -27,7 +33,7 @@ class PetListView(generic.ListView):
             1: 'dog',
             2: 'bird'
         }
-        
+
         context = super(PetListView, self).get_context_data(**kwargs)
         context['pet_category'] = css_class[self.CATEGORY]
         context['pet_category_verbose'] = self.CATEGORY_VERBOSE
